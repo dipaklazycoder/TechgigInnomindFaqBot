850691161da436481d7f8fddbe96965e5b2605e2
dipaklazycoder <dipaksharma9089@gmail.com>
2018-07-09 19:28:14 +0530

    Welcome message

--- TechgigInnomindsBot/Controllers/MessagesController.cs
+++ TechgigInnomindsBot/Controllers/MessagesController.cs
@@ -1,4 +1,6 @@
-﻿using System.Net;
+﻿using System;
+using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
@@ -38,9 +40,20 @@ namespace TechgigInnomindsBot
             }
             else if (messageType == ActivityTypes.ConversationUpdate)
             {
-                // Handle conversation state changes, like members being added and removed
-                // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
-                // Not available in all channels
+                IConversationUpdateActivity update = message;
+                var client = new ConnectorClient(new Uri(message.ServiceUrl), new MicrosoftAppCredentials());
+                if (update.MembersAdded != null && update.MembersAdded.Any())
+                {
+                    foreach (var newMember in update.MembersAdded)
+                    {
+                        if (newMember.Id != message.Recipient.Id)
+                        {
+                            var reply = message.CreateReply();
+                            reply.Text = "Welcome to Innomind Tech Bot";
+                            client.Conversations.ReplyToActivityAsync(reply);
+                        }
+                    }
+                }
             }
             else if (messageType == ActivityTypes.ContactRelationUpdate)
             {
